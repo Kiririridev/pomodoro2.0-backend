a6dd6281d00a5bdd4728c8e8f1a86c80b7cec0a5
Kiririridev <bartek.kirej@gmail.com>
2019-03-25 23:32:11 +0100

    Split PomodoroPostController to Controller and DatabaseAPI

--- src/main/java/pomodoro/kiririri/pomodorobackend/controllers/PomodoroPostController.java
+++ src/main/java/pomodoro/kiririri/pomodorobackend/controllers/PomodoroPostController.java
@@ -1,56 +1,30 @@
 package pomodoro.kiririri.pomodorobackend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import pomodoro.kiririri.pomodorobackend.db.PomodoroDataSource;
+import pomodoro.kiririri.pomodorobackend.db.PomodoroDatabaseAPI;
 import pomodoro.kiririri.pomodorobackend.dto.Pomodoro;
-import pomodoro.kiririri.pomodorobackend.rowmappers.PomodoroRowMapper;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RestController
 public class PomodoroPostController {
 
-	private static final String INSERT_POMODOROS = "INSERT INTO POMODOROS (USER_ID, POMODORO_DATE, LENGTH, DESCRIPTION, TAG) VALUES (?, ? ,?, ?, ?)";
-	private static final String SELECT_POMODOROS = "SELECT * FROM POMODOROS";
-
-	@Value("${spring.datasource.url}")
-	private String dbURL;
-	@Value("${spring.datasource.username}")
-	private String user;
-	@Value("${spring.datasource.password}")
-	private String password;
-
 	@Autowired
-	private PomodoroDataSource pomodoroDataSource;
-	private JdbcTemplate jdbcTemplate;
-
-	@PostConstruct
-	public void init() {
-		jdbcTemplate = new JdbcTemplate(pomodoroDataSource);
-	}
+	private PomodoroDatabaseAPI databaseAPI;
 
 	@PostMapping("/pomodoropost")
 	public void pomodoroPost(@RequestBody Pomodoro pomodoro) {
 
-		jdbcTemplate.update(INSERT_POMODOROS,
-				pomodoro.getUserId(),
-				pomodoro.getPomodoroDate(),
-				pomodoro.getLength(),
-				pomodoro.getDescription(),
-				pomodoro.getTag());
+		databaseAPI.persistPomodoroInDatabase(pomodoro);
 	}
 
 	@GetMapping("/pomodoroget")
 	public List<Pomodoro> pomodoroGet() {
-		List<Pomodoro> pomodoros = jdbcTemplate.query(SELECT_POMODOROS, new PomodoroRowMapper());
 
-		return pomodoros;
+		return databaseAPI.selectAllPomodorosFromDatabase();
 	}
 }
---
+++ src/main/java/pomodoro/kiririri/pomodorobackend/db/PomodoroDatabaseAPI.java
@@ -0,0 +1,51 @@
+package pomodoro.kiririri.pomodorobackend.db;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import pomodoro.kiririri.pomodorobackend.dto.Pomodoro;
+import pomodoro.kiririri.pomodorobackend.rowmappers.PomodoroRowMapper;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+public class PomodoroDatabaseAPI {
+
+	private static final String INSERT_POMODOROS = "INSERT INTO POMODOROS (USER_ID, POMODORO_DATE, LENGTH, DESCRIPTION, TAG) VALUES (?, ? ,?, ?, ?)";
+	private static final String SELECT_POMODOROS = "SELECT * FROM POMODOROS";
+
+	@Value("${spring.datasource.url}")
+	private String dbURL;
+	@Value("${spring.datasource.username}")
+	private String user;
+	@Value("${spring.datasource.password}")
+	private String password;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private PomodoroDataSource pomodoroDataSource;
+
+	@PostConstruct
+	public void init() {
+		jdbcTemplate = new JdbcTemplate(pomodoroDataSource);
+	}
+
+	public void persistPomodoroInDatabase(Pomodoro pomodoro) {
+
+		jdbcTemplate.update(INSERT_POMODOROS,
+				pomodoro.getUserId(),
+				pomodoro.getPomodoroDate(),
+				pomodoro.getLength(),
+				pomodoro.getDescription(),
+				pomodoro.getTag());
+	}
+
+	public List<Pomodoro> selectAllPomodorosFromDatabase() {
+		List<Pomodoro> pomodoros = jdbcTemplate.query(SELECT_POMODOROS, new PomodoroRowMapper());
+
+		return pomodoros;
+	}
+}
